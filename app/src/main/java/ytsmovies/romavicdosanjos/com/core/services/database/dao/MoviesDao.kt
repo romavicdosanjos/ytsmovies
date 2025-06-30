9e212b5a1d1a26bcbfa58ad0330c007ee1ed69ba
Romavic dos Anjos <romavicdosanjos@gmail.com>
2025-06-30 22:09:46 +0100

    Refactor: Improve ListMoviesScreen and Database Handling
    This commit refactors the `ListMoviesScreen` to improve its structure and UI, and updates the `MoviesDao` for better database conflict resolution.
    
    **ListMoviesScreen:**
    - Consolidated `LoadingView`, `ErrorView`, and `MoviesListView` into the main `ListMoviesScreen` Composable.
    - Simplified the UI by removing the `Card` and text details for each movie item, now only displaying the movie cover image.
    - Applied a `RoundedCornerShape` to the movie cover images for a more polished look.
    - Removed unused `Log` import.
    
    **MoviesDao:**
    - Added `OnConflictStrategy.REPLACE` to the `addMovies` function to handle potential conflicts when inserting movie data into the database. This ensures that existing movie entries are updated if new data for the same movie is inserted.

--- app/src/main/java/ytsmovies/romavicdosanjos/com/core/services/database/dao/MoviesDao.kt
+++ app/src/main/java/ytsmovies/romavicdosanjos/com/core/services/database/dao/MoviesDao.kt
@@ -2,6 +2,7 @@ package ytsmovies.romavicdosanjos.com.core.services.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import ytsmovies.romavicdosanjos.com.features.list_movies.domain.entity.MoviesEntity
 
@@ -11,6 +12,6 @@ interface MoviesDao {
     @Query("SELECT * FROM MoviesEntity")
     suspend fun getMovies(): List<MoviesEntity>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addMovies(data: List<MoviesEntity>)
 }
--- app/src/main/java/ytsmovies/romavicdosanjos/com/features/list_movies/presentation/ListMoviesScreen.kt
+++ app/src/main/java/ytsmovies/romavicdosanjos/com/features/list_movies/presentation/ListMoviesScreen.kt
@@ -1,20 +1,17 @@
 package ytsmovies.romavicdosanjos.com.features.list_movies.presentation
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,91 +20,66 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import org.koin.compose.viewmodel.koinViewModel
 import ytsmovies.romavicdosanjos.com.core.services.helpers.DataUiState
-import ytsmovies.romavicdosanjos.com.features.list_movies.domain.entity.MoviesEntity
 
 @Composable
 fun ListMoviesScreen() {
     val viewModel: ListMoviesViewModel = koinViewModel()
     val state by viewModel.moviesState.collectAsState()
 
-    when (state) {
-        is DataUiState.Loading -> LoadingView()
-        is DataUiState.Error -> ErrorView((state as DataUiState.Error).exception.message)
-        is DataUiState.Success -> MoviesListView((state as DataUiState.Success<List<MoviesEntity>>).data)
-    }
-}
-
-@Composable
-fun LoadingView() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-        CircularProgressIndicator()
-    }
-}
+        when (state) {
+            is DataUiState.Loading -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
 
-@Composable
-fun ErrorView(message: String?) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = message ?: "Unknown error", color = MaterialTheme.colorScheme.error)
+            is DataUiState.Error -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = (state as DataUiState.Error).exception.message ?: "Unknown error",
+                        color = MaterialTheme.colorScheme.error
+                    )
+                }
+            }
 
-        Log.e("Romavic", "$message")
-
-    }
-}
-
-@Composable
-fun MoviesListView(movies: List<MoviesEntity>) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp),
-        contentPadding = PaddingValues(8.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        items(movies.size) { index ->
-            val movie = movies[index]
-            MovieItem(movie)
-        }
-    }
-}
-
-@Composable
-fun MovieItem(movie: MoviesEntity) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(240.dp),
-        elevation = CardDefaults.cardElevation(4.dp)
-    ) {
-        Column(modifier = Modifier.padding(8.dp)) {
-            Image(
-                painter = rememberAsyncImagePainter(model = movie.largeCoverImage),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(140.dp)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = movie.titleEnglish,
-                style = MaterialTheme.typography.titleMedium,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text("Year: ${movie.year}", style = MaterialTheme.typography.bodyMedium)
-            Text("Rating: ${movie.rating}", style = MaterialTheme.typography.bodyMedium)
+            is DataUiState.Success -> {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                    contentPadding = PaddingValues(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    items((state as DataUiState.Success).data.count()) { index ->
+                        val movie = (state as DataUiState.Success).data[index]
+                        Image(
+                            painter = rememberAsyncImagePainter(model = movie.largeCoverImage),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(240.dp)
+                                .clip(RoundedCornerShape(8.dp))
+                        )
+                    }
+                }
+            }
         }
     }
 }
